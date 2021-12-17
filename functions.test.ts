2091c69681423a2b73461b30bc53368d4e7f3253
Senad Zenkovic <zenkovic88@gmail.com>
2021-12-17 10:09:24 -0700

    complete unit tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,23 @@
 const {shuffleArray} = require('./utils')
 
+const testArr = [1,2,3,4,5]
+
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test('should return an array', ()=>{
+        let returnedItemLength = shuffleArray(testArr).length
+        let testArrLength = testArr.length
+        expect(returnedItemLength).toEqual(testArrLength)
+    })
+
+    it('should be in a different order than original', ()=>{
+        let returnedItemArray = shuffleArray(testArr)
+        expect(returnedItemArray).not.toEqual(testArr)
+    })
 })
+
+
+// test('Title shows up when page loads', async () => {
+//     const title = await driver.findElement(By.id('title'))
+//     const displayed = await title.isDisplayed()
+//     expect(displayed).toBe(true)
+// })
